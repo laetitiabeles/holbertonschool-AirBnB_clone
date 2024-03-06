e0306d4268ede39b255395b8d1ae49045bd2b0db
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-06 13:37:57 +0100

    add condition for kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -13,9 +13,16 @@ class BaseModel:
              args: set of arguments
              kwargs: set of arguments with keywords
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ Base Model string """
