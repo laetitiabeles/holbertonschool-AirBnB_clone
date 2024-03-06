099f7fe002fcd92d85cba77c2f49de7ba41e736f
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-06 13:52:18 +0100

    add better documentation

--- models/__init__.py
+++ models/__init__.py
@@ -6,7 +6,36 @@ from the storage file.
 """
 
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 
 storage = FileStorage()
-
 storage.reload()
+
+classes = [
+    "BaseModel",
+    "User",
+    "State",
+    "Place",
+    "City",
+    "Amenity",
+    "Review"
+]
+
+int_attrs = [
+    "number_rooms",
+    "number_bathrooms",
+    "max_guest",
+    "price_by_night"
+]
+
+float_attrs = [
+    "latitude",
+    "longitude"
+]
