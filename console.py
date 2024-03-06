8840a5b0e3b8c6ccaa2f29bd26aeaac2185a96e0
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-06 15:13:08 +0100

    add do_create instance

--- console.py
+++ console.py
@@ -2,7 +2,8 @@
 """ Console module """
 
 import cmd
-
+import shlex
+import models
 
 class HBNBCommand(cmd.Cmd):
     """ Handle console monitor class
@@ -23,6 +24,22 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Empty line"""
         pass
+
+    def do_create(self, line):
+        if not line:
+            print("** class name missing **")
+        return
+
+        args = shlex.split(line)
+        class_name = args[0]
+
+        if line not in models.classes:
+            print("** class doesn't exist **")
+            return
+
+        instance = getattr(models, class_name)()
+        instance.save()
+        print(instance.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
