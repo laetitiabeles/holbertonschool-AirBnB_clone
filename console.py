3c91dbca80c22c20d432a8230e3b4c018c4bfa88
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-06 14:24:27 +0100

    add commands to quit the program

--- console.py
+++ console.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+""" Console module """
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Handle console monitor class
+    Args:
+        cmd: cmd class inheritance
+    """
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """EOF command to exit the program"""
+        print('')
+        return True
+
+    def emptyline(self):
+        """Empty line"""
+        pass
