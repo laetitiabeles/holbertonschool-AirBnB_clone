5619791127d2a2097e210ab9b2b42ca3f72ed292
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-08 14:13:26 +0100

    fixing args in all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,7 +32,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, line):
         """
         Returns the complete __objects dictionary, exposing all stored instances.
 
