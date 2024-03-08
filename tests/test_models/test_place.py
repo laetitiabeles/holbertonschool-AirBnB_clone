7e4b8c48c396ba10582af1491f0849301d3d1b0f
MLK59 <abdelmalekmhamed@gmail.com>
2024-03-08 10:16:28 +0100

    build: adding testing (place)

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Defines unittests for models/place.py.
+
+Unittest classes:
+    TestPlace_instantiation
+    TestPlace_save
+    TestPlace_to_dict
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.place import Place
+
+
+class TestPlace_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Place class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(Place, type(Place()))
+
+
+class TestPlace_save(unittest.TestCase):
+    """Unittests for testing save method of the Place class."""
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def test_one_save(self):
+        pl = Place()
+        sleep(0.05)
+        first_updated_at = pl.updated_at
+        pl.save()
+        self.assertLess(first_updated_at, pl.updated_at)
+
+
+class TestPlace_to_dict(unittest.TestCase):
+    """Unittests for testing to_dict method of the Place class."""
+
+    def test_to_dict_type(self):
+        self.assertTrue(dict, type(Place().to_dict()))
+
+
+if __name__ == "__main__":
+    unittest.main()
