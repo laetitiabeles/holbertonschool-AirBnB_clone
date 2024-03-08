1e25008dfd4a2b686ef24ed2fe3582a82720cefe
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-08 15:24:59 +0100

    add tests file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,73 +1,97 @@
 #!/usr/bin/python3
+""" Unit tests for the FileStorage class """
 
-import unittest
-from datetime import datetime
-from time import sleep
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-from os import path
+import os
+import unittest
+import json
 
 
-class TestStorage(unittest.TestCase):
-    # def test_file_path(self):
-    #     self.assertIsNone(FileStorage.__file_path)
+class TestFileStorage(unittest.TestCase):
+    """ Test class for the FileStorage class """
 
-    def test_file_path1(self):
-        self.assertTrue(path.exists(FileStorage._FileStorage__file_path))
-
-    def test_update_now(self):
+    def test_file_path(self):
+        """  Test file path """
         model = BaseModel()
-        original_updated_at = model.updated_at
-        original_created_at = model.created_at
-        sleep(1)
         model.save()
-        self.assertNotEqual(original_updated_at, model.updated_at)
-        self.assertTrue(original_created_at, model.created_at)
-        self.assertNotEqual(model.updated_at, model.created_at)
+        self.assertEqual(FileStorage._FileStorage__file_path, "file.json")
+        os.remove("file.json")
 
-    def test_save(self):
+    def test_objects_dict(self):
+        """ Test objects dictionary """
         model = BaseModel()
-        self.assertIsNone(model.save())
-
-    def test_update_type(self):
-        model = BaseModel()
-        self.assertTrue(type(model.updated_at) is datetime)
-
-    def setUp(self):
-        """instance for testing"""
-        self.storage = FileStorage()
-
-    def tearDown(self):
-        """clean up after testing"""
-        if os.path.exists(FileStorage.__file_path):
-            os.remove(FileStorage.__file_path)
-
-    def test_pycode(self):
-        style = pycodestyle.StyleGuide(quiet=True)
-        files = [
-            'models/engine/file_storage.py'
-            'tests/test_models/test_engine/test_file_storage.py'
-        ]
-        result = style.check_files(files)
-        self.assertEqual(
-            result.total_errors, 0, "PEP 8 style issues found"
-        )
+        model.save()
+        self.assertEqual(type(FileStorage._FileStorage__objects), dict)
+        os.remove("file.json")
 
     def test_all(self):
-        """test all method"""
-        test_dict = self.storage.all()
-        self.assertIsInstance(test_dict, dict)
+        """ Test all filestorage """
+        storage = FileStorage()
+        instance = storage.all()
+        self.assertIsNotNone(instance)
+        self.assertIsInstance(instance, dict)
 
     def test_new(self):
-        """tests if new method adds object correctly"""
-        user = User()
-        self.storage.new(user)
-        key = "User." + str(user.id)
-        self.assertTrue(key in self.storage.all())
+        """ Test new filestorage """
+        FileStorage._FileStorage__objects = {}
+        objects = FileStorage._FileStorage__objects
+        model = BaseModel()
+        FileStorage.new(FileStorage, model)
+        self.assertNotEqual(
+            objects[f"{model.__class__.__name__}.{model.id}"], None)
+
+    def test_save(self):
+        """ Test save filestorage """
+        bm = BaseModel()
+        storage = FileStorage()
+        storage.new(bm)
+        storage.save()
+        with open("file.json", "r") as file:
+            self.assertIn("BaseModel." + bm.id, file.read())
+        os.remove("file.json")
 
     def test_reload(self):
-        """tests deserialization of objects"""
-        user = User() in self.storage.all()
+        """ Test reload filestorage """
+        bm = BaseModel()
+        storage = FileStorage()
+        storage.new(bm)
+        storage.save()
+        storage.reload()
+        FileStorage._FileStorage__objects
+        self.assertIn("BaseModel." + bm.id, FileStorage._FileStorage__objects)
+        os.remove("file.json")
 
-    if __name__ == '__main__':
-        unittest.main()
+    def test_init(self):
+        """ Test instance init"""
+        file = FileStorage()
+        self.assertIsInstance(file._FileStorage__objects, dict)
+        self.assertIsInstance(file._FileStorage__file_path, str)
+
+    def test_reload2(self):
+        """ Test reload filestorage """
+        storage = FileStorage()
+        with self.assertRaises(TypeError):
+            storage.reload(None)
+
+    def test_reload3(self):
+        """ Test reload filestorage """
+        test_dict = {"BaseModel.test": {
+            "id": "test", "created_at": "2023-10-23T14:10:06.000000",
+            "updated_at": "2023-10-23T14:10:06.000000",
+            "__class__": "BaseModel"
+            }}
+
+        with open(FileStorage._FileStorage__file_path, "w") as file:
+            json.dump(test_dict, file)
+
+        storage = FileStorage()
+        storage.reload()
+
+        loaded_obj = storage.all()["BaseModel.test"]
+        self.assertIsInstance(loaded_obj, BaseModel)
+        self.assertEqual(loaded_obj.id, "test")
+        self.assertEqual(str(loaded_obj.created_at), "2023-10-23 14:10:06")
+        self.assertEqual(str(loaded_obj.updated_at), "2023-10-23 14:10:06")
+
+        os.remove(FileStorage._FileStorage__file_path)
