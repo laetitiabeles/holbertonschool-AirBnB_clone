47c27cc452ce61dd15f4db7fe93a80de09e2afa5
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-08 15:18:46 +0100

    fixing state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """State Model"""
+
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """State class Model that shows state imformation"""
-    name = ""
+    """ State class
+    Args:
+        BaseModel : inheritance
+    """
+    name = str("")
