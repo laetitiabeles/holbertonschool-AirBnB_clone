74f07ba9c37129a279601efd2f26e2e9cfe4eea4
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-08 15:34:17 +0100

    fixing tests for place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,61 +1,28 @@
 #!/usr/bin/python3
-"""
-Defines unittests for models/place.py.
+""" Unittest for Place """
 
-Unittest classes:
-    TestPlace_instantiation
-    TestPlace_save
-    TestPlace_to_dict
-"""
-import os
-import models
 import unittest
-from datetime import datetime
-from time import sleep
+from models.base_model import BaseModel
 from models.place import Place
 
 
-class TestPlace_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Place class."""
+class testPlace(unittest.TestCase):
+    """ Test class for Place """
 
-    def test_no_args_instantiates(self):
-        self.assertEqual(Place, type(Place()))
+    def test_class(self):
+        """ Validate the attributes. """
+        with self.subTest(msg='Inheritance'):
+            self.assertTrue(issubclass(Place, BaseModel))
 
-
-class TestPlace_save(unittest.TestCase):
-    """Unittests for testing save method of the Place class."""
-
-    @classmethod
-    def setUp(self):
-        try:
-            os.rename("file.json", "tmp")
-        except IOError:
-            pass
-
-    def tearDown(self):
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-        try:
-            os.rename("tmp", "file.json")
-        except IOError:
-            pass
-
-    def test_one_save(self):
-        pl = Place()
-        sleep(0.05)
-        first_updated_at = pl.updated_at
-        pl.save()
-        self.assertLess(first_updated_at, pl.updated_at)
-
-
-class TestPlace_to_dict(unittest.TestCase):
-    """Unittests for testing to_dict method of the Place class."""
-
-    def test_to_dict_type(self):
-        self.assertTrue(dict, type(Place().to_dict()))
-
-
-if __name__ == "__main__":
-    unittest.main()
+        with self.subTest(msg='Attributes'):
+            self.assertIsInstance(Place.city_id, str)
+            self.assertIsInstance(Place.user_id, str)
+            self.assertIsInstance(Place.name, str)
+            self.assertIsInstance(Place.description, str)
+            self.assertIsInstance(Place.number_rooms, int)
+            self.assertIsInstance(Place.number_bathrooms, int)
+            self.assertIsInstance(Place.max_guest, int)
+            self.assertIsInstance(Place.price_by_night, int)
+            self.assertIsInstance(Place.latitude, float)
+            self.assertIsInstance(Place.longitude, float)
+            self.assertIsInstance(Place.amenity_ids, list)
