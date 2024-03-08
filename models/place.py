7b2dd653077a2bcb318052f38cdb975f24ceb03c
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-08 15:30:09 +0100

    fixing documentation

--- models/place.py
+++ models/place.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """ create class Place"""
+
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """ class that inherit from BaseModel """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    """Place class
+    Args:
+        BaseModel : inheritance
+    """
+    city_id = str("")
+    user_id = str("")
+    name = str("")
+    description = str("")
+    number_rooms = int(0)
+    number_bathrooms = int(0)
+    max_guest = int(0)
+    price_by_night = int(0)
+    latitude = float(0.0)
+    longitude = float(0.0)
     amenity_ids = []
